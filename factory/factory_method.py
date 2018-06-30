8be20ed174767830e28b589de7faac48efe024cd
Volodymyr Yahello <vyahello@gmail.com>
2018-07-01 00:03:03 +0300

    Add factory method design pattern

---
+++ factory/factory_method.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+
+class Pet(ABC):
+    """Abstraction of a pet."""
+
+    @abstractmethod
+    def speak(self) -> str:
+        pass
+
+
+class Dog(Pet):
+    """A simple dog class."""
+
+    def __init__(self, name: str) -> None:
+        self._name: str = name
+
+    def speak(self) -> str:
+        return f'{self._name} says Woof!'
+
+
+class Cat(Pet):
+    """A simple cat class."""
+
+    def __init__(self, name: str) -> None:
+        self._name: str = name
+
+    def speak(self) -> str:
+        return f'{self._name} says Meow!'
+
+
+def get_pet(pet: str) -> Pet:
+    """The factory method."""
+
+    return dict(dog=Dog("Hope"), cat=Cat("Faith")).get(pet)
