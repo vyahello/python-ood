e7303ad94eddc1e45568e1259d06ab2c5e6b8e4e
Volodymyr Yahello <vyahello@gmail.com>
2018-07-01 13:30:40 +0300

    Add singleton design pattern

---
+++ factory/singleton.py
@@ -0,0 +1,51 @@
+from typing import Any
+
+
+class SimpleSingleton(object):
+    """Make all instances the same object."""
+
+    def __new__(cls) -> 'SimpleSingleton':
+        if not hasattr(cls, '_instance'):
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+
+class Borg(object):
+    """Borg class making class attributes global.
+    Safe the same state of all instances but instances are all different."""
+
+    _shared_state: dict = {}  # Attribute dictionary
+
+    def __init__(self) -> None:
+        # Make it an attribute dictionary
+        self.__dict__ = self._shared_state
+
+
+class BorgSingleton(Borg):
+    """This class shares all its attribute among its instances. Store the same state."""
+    # Makes a singleton an object-oriented global variable
+
+    def __init__(self, **kwargs: Any) -> None:
+        Borg.__init__(self)
+        # Update the attribute dictionary by inserting a new key-value pair
+        self._shared_state.update(kwargs)
+
+    def __str__(self) -> str:
+        # Return the attribute dict for printing
+        return str(self._shared_state)
+
+# Instantiate the same singleton object
+s_one = SimpleSingleton()
+s_two = SimpleSingleton()
+
+print(id(s_one))
+print(id(s_two))
+print(s_one is s_two)
+
+# Create a singleton object and add out first acronym
+x = BorgSingleton(HTTP='Hyper Text Transfer Protocol')
+print(x)
+
+# Create another singleton which will add to the existent dict attribute
+y = BorgSingleton(SNMP='Simple Network Management Protocol')
+print(y)
