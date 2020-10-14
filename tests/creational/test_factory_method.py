3917a253ca27dacfcc35adda36ace548ab826d7d
Volodymyr Yahello <vyahello@gmail.com>
2020-10-14 22:54:31 +0300

    Use unittest pytestmark for factory method tests

--- tests/creational/test_factory_method.py
+++ tests/creational/test_factory_method.py
@@ -13,6 +13,8 @@ from patterns.creational.factory_method import (
 )
 from tests.marker import unittest
 
+pytestmark = unittest
+
 
 @pytest.fixture(scope="module")
 def circle() -> Shape:
@@ -24,12 +26,10 @@ def square() -> Shape:
     return Square()
 
 
-@unittest
 def test_draw_circle(circle: Shape) -> None:
     assert circle.draw() == "Circle.draw"
 
 
-@unittest
 def test_draw_square(square: Shape) -> None:
     assert square.draw() == "Square.draw"
 
