f230985ebebe26742253e9189a63983c261f2920
Volodymyr Yahello <vyahello@gmail.com>
2020-12-22 11:54:25 +0200

    Add creational "builder" unit tests

--- tests/creational/test_builder.py
+++ tests/creational/test_builder.py
@@ -1,3 +1,4 @@
+# pylint:disable=protected-access
 import pytest
 
 from patterns.creational.builder import (
@@ -30,6 +31,21 @@ def test_car_summary(car: Machine) -> None:
 
 def test_skylark_builder_machine(skylark_builder: Builder) -> None:
     assert isinstance(skylark_builder.machine(), Machine)
+
+
+def test_skylark_builder_add_model(skylark_builder: Builder) -> None:
+    skylark_builder.add_model()
+    assert skylark_builder._car.model == "SkyBuilder model"
+
+
+def test_skylark_builder_add_tires(skylark_builder: Builder) -> None:
+    skylark_builder.add_tires()
+    assert skylark_builder._car.tires == "Motosport tires"
+
+
+def test_skylark_builder_add_engine(skylark_builder: Builder) -> None:
+    skylark_builder.add_engine()
+    assert skylark_builder._car.engine == "GM Motors engine"
 
 
 def test_director_release_machine(director: Director) -> None:
